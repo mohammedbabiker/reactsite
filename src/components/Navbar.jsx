c64eb1b36f213213925ad58375597faac1fed8b0
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-01 15:35:57 -0700

    style navbar and hover effect, complete navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,35 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../images/logo.png";
 import { links } from "../data";
 import { GoMoveToStart } from "react-icons/go";
+import { MdOutlineClose } from "react-icons/md";
 import "./navbar.css";
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
+
   return (
     <nav>
       <div className="container nav__container">
         <Link to="/" className="logo">
           <img src={Logo} alt="nav logo" />
         </Link>
-        <ul className="nav__links">
+        <ul className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}>
           {links.map(({ name, path }, index) => {
             return (
-              <li>
-                <NavLink to={path}>{name}</NavLink>
+              <li key={index}>
+                <NavLink to={path} className={({isActive}) => isActive ? "active-nav" : ""}>{name}</NavLink>
               </li>
             );
           })}
         </ul>
-        <button className="nav__toggle-btn">
-          <GoMoveToStart />
+        <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+          {
+            isNavShowing ? <MdOutlineClose/>: <GoMoveToStart />
+          }
         </button>
       </div>
     </nav>
