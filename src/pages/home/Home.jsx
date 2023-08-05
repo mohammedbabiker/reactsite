e6753f18f94dbece6cb6bf52c2d27969795e1f5d
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-05 21:29:30 +0300

    add section FAQS,FAQ file and update there css,jsx at home, update values key l,22

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
 import Values from "./../../components/Values";
 import "./home.css";
+import FAQs from "./../../components/FAQs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MainHeader />
       <Programs />
       <Values />
+      <FAQs />
     </>
   );
 };
