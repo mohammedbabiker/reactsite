0ea2d71588b15f51e8f3ea40837779789a8744d9
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-06 21:23:10 +0300

    cmopleate contact cmoponents, css

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,48 @@
-import "./contact.css"
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+        lorem Ips et sapien et dolor sit amet, consectetur adipis ac metus et
+        dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit
+        amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet,
+        consectetur adip.
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a
+              href="mailto:mobabiekrbabai20@gmail.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="https://wa.me/+996542160187"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IoLogoWhatsapp />
+            </a>
+            <a
+              href="https://m.me/mohammedbabai.16"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <BsMessenger />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
