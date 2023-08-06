4fa053f4efa627f88a7cbd3d7f1a1dd35ab17c7c
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-06 22:04:42 +0300

    update gallery cmoponents & g.css its work cool

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,35 @@
-import "./gallery.css"
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import "./gallery.css";
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const galleryLength = 15;
+  const images = [];
 
-export default Gallery
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+
+  return (
+    <>
+      <Header title="Gallery" image={HeaderImage}>
+        lorem Ips et dolor sit amet, consectetur adipis occ tempor invidunt ut
+        labore et dolore magna aliqu sapient voluptatem. Lorem ipsum dolor sit
+        amet, consectetur.
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Img ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
