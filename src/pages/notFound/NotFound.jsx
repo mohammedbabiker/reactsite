ab1c63117d7eb9f3e956d2a53efcffe579281df3
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-06 22:19:12 +0300

    update notfound components & css

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,9 +1,17 @@
-import "./notFound.css"
+import { Link } from "react-router-dom";
+import "./notFound.css";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <section>
+      <div className="container notfound__container">
+        <h2>Page Not Found</h2>
+        <Link to="/" className="btn">
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default NotFound
+export default NotFound;
