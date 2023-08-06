8b0538aaece9f55939f165f1c7013381caafe219
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-06 23:21:39 +0300

    update plans components & css

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,9 +1,43 @@
-import "./plans.css"
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../UI/Card";
+import { plans } from "../../data";
+
+import "./plans.css";
 
 const Plans = () => {
   return (
-    <div>Plans</div>
-  )
-}
+    <>
+      <Header title="Membership Plans" image={HeaderImage}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry standard dummy text ever
+        printed. Lorem Ipsum has.
+      </Header>
+      <section className="plans">
+        <div className="container plans__container">
+          {plans.map(({ id, name, desc, price, features }) => {
+            return (
+              <Card key={id} className="plan">
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1>
+                <h2>/mo</h2>
+                <h4>Features</h4>
+                {features.map(({ feature, available }, index) => {
+                  return (
+                    <p key={index} className={!available ? "disabled" : ""}>
+                      {feature}
+                    </p>
+                  );
+                })}
+                <button className="btn lg">Choose Plan</button>
+              </Card>
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Plans
+export default Plans;
