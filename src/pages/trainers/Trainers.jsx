4a5c1d666a608825d023767655086e5372daaec3
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-08-07 13:37:53 +0300

    update trainers components css & create trainer.jsx

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,9 +1,42 @@
-import "./trainers.css"
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_5.jpg";
+import { trainers } from "../../data";
+import { BsInstagram } from "react-icons/bs";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import Trainer from "../../components/Trainer";
+import "./trainers.css";
 
 const Trainers = () => {
   return (
-    <div>Trainers</div>
-  )
-}
+    <>
+      <Header title="Our Trainers" image={HeaderImage}>
+        Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dol acceptet null null null.
+      </Header>
+      <section className="trainers">
+        <div className="container trainers__container">
+          {trainers.map(({ id, image, name, job, socials }) => {
+            return (
+              <Trainer
+                key={id}
+                image={image}
+                name={name}
+                job={job}
+                socials={[
+                  { icon: <BsInstagram />, link: socials[0] },
+                  { icon: <AiOutlineTwitter />, link: socials[1] },
+                  { icon: <FaFacebookF />, link: socials[2] },
+                  { icon: <FaLinkedin />, link: socials[3] },
+                ]}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Trainers
+export default Trainers;
